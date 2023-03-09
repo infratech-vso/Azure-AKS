b337250fc89e10fccd38acfd281c65238061df40
Valter Soares <82425070+infratech-vso@users.noreply.github.com>
2023-03-08 21:36:25 -0300

    Add files via upload

---
+++ AzStopStart.ps1
@@ -0,0 +1,23 @@
+Param
+(
+    [Parameter (Mandatory= $true)]
+    [string]$AKSResourceGroup,
+    [Parameter (Mandatory- $true)]
+    [string]$AKSName,
+    [Parameter (Mandatory- $true)]
+    [string]$subscriptionID
+    [Parameter (Mandatory= $true,HelpMessage="start or stop")]
+    [string]$Action
+)
+
+Connect-AzAccount -Identity
+Set-AzContext -SubscriptionId $subscriptionID
+
+if (SActig -eq "start") {
+    Get-AzAksCluster -Name $AKSName -ResourceGroupName $AKSResourceGroup
+    Start-AzAksCluster -Name $AKSName -ResourceGroupName $AKSResourceGroup
+}
+elseif ($Action -eq "stop") {
+    Get-AzAksCluster -Name $AKSName -ResourceGroupName $AKSResourceGroup
+    Stop-AzAksCluster -Name $AKSName -ResourceGroupName $AKSResourceGroup
+}
